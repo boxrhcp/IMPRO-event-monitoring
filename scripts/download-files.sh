a1290d16c89091f06f873843e124b412db890cf0
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2019-11-24 17:10:39 +0100

    download script created

---
+++ scripts/download-files.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import urllib.request
+import zipfile
+import os
+
+page = urllib.request.urlopen('http://data.gdeltproject.org/gdeltv2/lastupdate.txt')
+content = page.read()
+text = content.decode('utf-8')
+gkg_url = text.split(' ')[-1]
+filename = gkg_url.split('/')[-1].replace('\n','')
+print(filename)
+urllib.request.urlretrieve(gkg_url,filename)
+with zipfile.ZipFile(filename, 'r') as zip_ref:
+    zip_ref.extractall('../source')
+os.remove(filename)
+
